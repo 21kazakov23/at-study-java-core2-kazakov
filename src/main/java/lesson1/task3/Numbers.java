9dd12b7f64dfb15d87f192d04a54f38ccd256a9a
21kaz <21kazakov23@gmail.com>
2021-05-10 01:46:02 +0300

    home1

--- src/main/java/lesson1/task3/Numbers.java
+++ src/main/java/lesson1/task3/Numbers.java
@@ -2,7 +2,16 @@ package lesson1.task3;
 
 public class Numbers {
     public static void main(String[] args) {
-        boolean
-
+        Numbers numbers = new Numbers();
+        System.out.println(numbers.getNumbers(14));
+        System.out.println(numbers.getNumbers(19));
     }
+
+    boolean getNumbers(int number) {
+        int a = number % 2;
+        return a == 1;
+    }
+
+
 }
+
