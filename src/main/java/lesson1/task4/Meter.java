32e4aa9daa89d3be8d77f1b2b17aa9bf6f3e33eb
21kaz <21kazakov23@gmail.com>
2021-05-10 02:04:20 +0300

    home1

---
+++ src/main/java/lesson1/task4/Meter.java
@@ -0,0 +1,15 @@
+package lesson1.task4;
+
+public class Meter {
+    public static void main(String[] args) {
+        Meter meter = new Meter();
+        System.out.println(meter.centimeter(115));
+        System.out.println(meter.centimeter(800));
+        System.out.println(meter.centimeter(278));
+
+    }
+    int centimeter(int a) {
+        int b = a / 100;
+        return b;
+    }
+}
