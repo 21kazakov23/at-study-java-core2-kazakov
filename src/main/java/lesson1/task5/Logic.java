b9ef27403fc684c9ed1f674152fc0a10b9409d12
21kaz <21kazakov23@gmail.com>
2021-05-10 15:39:34 +0300

    home2

--- src/main/java/lesson1/task5/Logic.java
+++ src/main/java/lesson1/task5/Logic.java
@@ -1,4 +1,22 @@
 package lesson1.task5;
 
 public class Logic {
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.first(false, false) + " " + logic.second(false, false));
+        System.out.println(logic.first(false, true)+ " " + logic.second(false, true));;
+        System.out.println(logic.first(true, false)+ " " + logic.second(true, false));;
+        System.out.println(logic.first(true, true)+ " " + logic.second(true, true));;
+
+
+    }
+
+    boolean first(boolean a, boolean b) {
+        return !(a || b);
+    }
+
+    boolean second(boolean a, boolean b) {
+        return !a && !b;
+    }
+
 }
