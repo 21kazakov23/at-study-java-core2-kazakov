da47ebf2832ef1b0e1b879a58ecbf61f4251cc77
21kaz <21kazakov23@gmail.com>
2021-05-17 20:16:24 +0300

    home2

---
+++ src/main/java/lections2/Arrays.java
@@ -0,0 +1,19 @@
+package lections2;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        // Одномерные массивы
+        int[] array;
+        int[] array2 = new int[3];
+        int[] array3 = {1,2,3,4,5,};
+
+        array3[1] = 4;
+        array3[3] = array3[2];
+
+
+        System.out.println(array3);
+
+    }
+
+}
