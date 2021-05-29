886da63411222d926a4a0669148cf286f7c4c92c
21kaz <21kazakov23@gmail.com>
2021-05-29 15:25:01 +0300

    home

---
+++ src/main/java/lesson3/task1/Calculator.java
@@ -0,0 +1,33 @@
+package lesson3.task1;
+
+public class Calculator {
+    /*
+    Складываемся
+     */
+    int addition(int numA, int numB) {
+        return numA + numB;
+    }
+
+    /*
+    Вычитаемся
+     */
+    int subtraction(int numA, int numB) {
+        return numA - numB;
+    }
+
+    /*
+    Размножимся
+     */
+    int multiplication(int numA, int numB) {
+        return numA * numB;
+    }
+
+    /*
+   Разделимся
+    */
+    int separation(int numA, int numB) {
+        return numA / numB;
+    }
+
+
+}
---
+++ src/main/java/lesson3/task1/CalculatorApplication.java
@@ -0,0 +1,15 @@
+package lesson3.task1;
+
+public class CalculatorApplication {
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addition(4,2));
+        System.out.println(calculator.multiplication(2,6));
+        System.out.println(calculator.separation(5,3));
+        System.out.println(calculator.subtraction(5,3));
+    }
+
+
+
+}
+
